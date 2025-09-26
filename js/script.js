a44bc0fae4774fba016bde5560517f56d885cd52
FreddieAlvarez <alvarez.freddie@ic.edu>
2025-09-25 19:37:32 -0500

    User Selection and stored answers

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 console.log("script.js connected!");
+
+let userAnswers = {}
+
+let buttons = document.querySelectorAll(".question-block .answer-btn");
+console.log(buttons);
+
+buttons.forEach(function(button) {
+    button.addEventListener("click", function() {
+        let questionBlock = button.closest(".question-block");
+        let allButtonsInBlock = questionBlock.querySelectorAll(".answer-btn");
+
+        allButtonsInBlock.forEach(function(btn) {
+            btn.classList.remove("selected");
+        });
+
+        button.classList.add("selected");
+
+        let selectedAnswer = button.getAttribute("data-answer");
+
+        let questionId = questionBlock.getAttribute("data-question-id");
+
+        userAnswers[questionId] = selectedAnswer;
+
+        console.log("Selected answer:", selectedAnswer);
+    });
+});
+

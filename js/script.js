2e44fb339337349bbd084e73494a03ac7017319d
FreddieAlvarez <alvarez.freddie@ic.edu>
2025-09-25 20:41:08 -0500

    Updated code

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 console.log("script.js connected!");
 
-let userAnswers = {}
+let userAnswers = {};
 
 let buttons = document.querySelectorAll(".question-block .answer-btn");
 console.log(buttons);
@@ -26,3 +26,47 @@ buttons.forEach(function(button) {
     });
 });
 
+function displayResult() {
+    let stylePoints = {
+        "Goblin Barrel": 0,
+        "Wizard": 0
+    };
+
+    for (let questionId in userAnswers) {
+        let questionBlock = document.querySelector(`.question-block[data-question-id="${questionId}"]`);
+        
+        let selectedButton = questionBlock.querySelector(`button[data-answer="${userAnswers[questionId]}"]`);
+
+        if (selectedButton) {
+            let style = selectedButton.getAttribute("data-style");
+            let points = parseInt(selectedButton.getAttribute("data-points"), 10);
+
+            stylePoints[style] += points;
+        }
+    }
+    let resultStyle = null;
+    if (stylePoints["Goblin Barrel"] > stylePoints["Wizard"]) {
+        resultStyle = "Goblin Barrel";
+    } else if (stylePoints["Wizard"] > stylePoints["Goblin Barrel"]) {
+        resultStyle = "Wizard";
+    } else {
+        resultStyle = "You're a mix";
+    }
+
+    let message = "";
+    if (resultStyle == "Goblin Barrel") {
+        message = "You are a Goblin Barrel!";
+    } else if (resultStyle == "Wizard") {
+        message = "You are a Wizard";
+    } else {
+        message = "You have style of both";
+    }
+
+    let resultContainer = document.getElementById("result-container");
+    let resultText = document.getElementById("result-text");
+    
+    resultText.textContent = message;
+    resultContainer.style.display = "block";
+}
+
+document.getElementById("show-results").addEventListener("click", displayResult);
